ed42a8d99cbdfadf68d347e2be3bf5f3e5dde533
Bartosz Maleta <bartosz.maleta@gmail.com>
2017-12-17 11:33:27 +0100

    User class added

--- src/main/java/com/example/adressbook/domain/User.java
+++ src/main/java/com/example/adressbook/domain/User.java
@@ -1,5 +1,60 @@
 package com.example.adressbook.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User
 {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+
+    public User(String firstname, String lastname)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("id:%s, %s, %s", id, firstname, lastname);
+    }
+
+    public User() {}
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
 }
